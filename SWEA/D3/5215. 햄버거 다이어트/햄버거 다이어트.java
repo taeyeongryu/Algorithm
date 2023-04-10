c7f5c1226230e4d2306d067c89333458e0de5c39
taeyeongryu <122500557+taeyeongryu@users.noreply.github.com>
2023-04-10 14:02:06 +0900

    [D3] Title: 햄버거 다이어트, Time: 186 ms, Memory: 36,732 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,63 +1,30 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
-	static class ing {
-		int cal;
-		int taste;
-
-		public ing(int taste, int cal) {
-			this.cal = cal;
-			this.taste = taste;
-		}
-	}
-
-	static int N;
-	static int L;
-	static int max;
-
-	static ing[] ings;
-	static boolean[] choice;
-
-	static void DFS(int depth) {
-		if (depth == N) {
-			int cal = 0;
-			int taste = 0;
-			for (int i = 0; i < ings.length; i++) {
-				if (choice[i] == true) {
-					cal += ings[i].cal;
-					taste += ings[i].taste;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int Test = sc.nextInt();
+		for (int tc = 1; tc <=Test; tc++) {
+			int N = sc.nextInt();
+			int L = sc.nextInt();
+			int T[] = new int[N+1];
+			int K[] = new int[N+1];
+			int dp[][] = new int[N+1][10001];
+			for (int i = 1; i <= N; i++) {
+				T[i]=sc.nextInt();
+				K[i]=sc.nextInt();
+			}
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= 10000; j++) {
+					if (K[i]>j) {
+						dp[i][j]=dp[i-1][j];
+					}
+					else {
+						dp[i][j]=Math.max(dp[i-1][j], dp[i-1][j-K[i]]+T[i]);
+					}
 				}
 			}
-			if (cal <= L) {
-				max = Math.max(max, taste);
-			}
-		} else {
-			choice[depth] = true;
-			DFS(depth + 1);
-			choice[depth] = false;
-			DFS(depth + 1);
+			System.out.printf("#%d %d%n",tc,dp[N][L]);
 		}
 	}
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-
-		for (int tc = 1; tc <= T; tc++) {
-			N = sc.nextInt();
-			L = sc.nextInt();
-			max = 0;
-			ings = new ing[N];
-			choice = new boolean[N];
-			for (int i = 0; i < N; i++) {
-				ings[i] = new ing(sc.nextInt(), sc.nextInt());
-			}
-			DFS(0);
-			System.out.printf("#%d %d%n",tc,max);
-
-		}
-	}
-
 }
