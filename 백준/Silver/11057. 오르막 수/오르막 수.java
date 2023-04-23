367cb40677eddc0e61157d44927246a2ed34b9ad
taeyeongryu <122500557+taeyeongryu@users.noreply.github.com>
2023-04-23 12:13:35 +0900

    [Silver I] Title: 오르막 수, Time: 128 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[][] dp = new int[10][1001];
+		for (int i = 0; i <= 9; i++) {
+			dp[i][1] = 1;
+		}
+		for (int j = 2; j <= N; j++) {
+			for (int i = 0; i <= 9; i++) {
+				for (int k = 0; k <= i; k++) {
+					dp[i][j] += dp[k][j - 1];
+					dp[i][j] %= 10007;
+				}
+			}
+		}
+//		for (int i = 0; i <= 9; i++) {
+//			for (int j = 1; j <= N; j++) {
+//				System.out.print(dp[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+		int result = 0;
+		for (int i = 0; i <= 9; i++) {
+			result += dp[i][N];
+		}
+		System.out.println(result%10007);
+	}
+}
