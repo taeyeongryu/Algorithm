afcd35ccb4d5a43401c4d8f740c302ea766f4401
taeyeongryu <122500557+taeyeongryu@users.noreply.github.com>
2023-04-25 15:02:16 +0900

    [Silver III] Title: N과 M (1), Time: 228 ms, Memory: 43040 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,40 +1,41 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int M;
-	static int[] arr;
-	static boolean[] visited;
-
-	static void backtracking(int depth) {
+	static int[] result;
+	static boolean[] choice;
+	static StringBuilder sb = new StringBuilder();
+	static void Back(int depth, int N, int M) {
 		if (depth == M) {
-			for (int i = 0; i < arr.length; i++) {
-				System.out.print(arr[i] + " ");
+			for (int i = 0; i < result.length; i++) {
+				sb.append(""+result[i]+" ");
 			}
-			System.out.println();
+			sb.append("\n");
 			return;
-		} else {
-			for (int i = 0; i < visited.length; i++) {
-				if (visited[i] == false) {
-					visited[i] = true;
-					arr[depth] = i + 1;
-					backtracking(depth + 1);
-					visited[i] = false;
-				}
-			}
 		}
 
+		for (int i = 1; i <= N; i++) {
+			if (choice[i] == false) {
+				choice[i] = true;
+				result[depth] = i;
+				Back(depth + 1, N, M);
+				choice[i] = false;
+			}
+		}
 	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		N = sc.nextInt();
-		M = sc.nextInt();
-		arr = new int[M];
-		visited = new boolean[N];
-		backtracking(0);
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		result = new int[M];
+		choice = new boolean[N + 1];
+		Back(0, N, M);
+		System.out.println(sb.toString());
 	}
-
 }
+
