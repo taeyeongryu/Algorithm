3e7dc8a5c1768e37a2fc555e359c6d0a52f3df1d
taeyeongryu <122500557+taeyeongryu@users.noreply.github.com>
2023-05-02 22:20:25 +0900

    [Gold III] Title: 소문난 칠공주, Time: 392 ms, Memory: 142740 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -3,70 +3,102 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	static char[][] map = new char[5][5];
-	static int[] dx = { 1, -1, 0, 0 };
-	static int[] dy = { 0, 0, -1, 1 };
+	static class Point {
+		int r;
+		int c;
 
-	static int[] combX = new int[25];
-	static int[] combY = new int[25];
+		public Point(int r, int c) {
+			super();
+			this.r = r;
+			this.c = c;
+		}
 
-	static int ans = 0;
+	}
+
+	static int[] dr = { 1, -1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+	static int[] result = new int[7];
+	static boolean[][] goal;
+	static char[][] map;
+	static int total = 0;
+
+	// 이다솜 파가 4명 이상인지
+	static boolean bfs() {
+		int cnt = 0;
+		Queue<Point> q = new LinkedList<>();
+		Point start = new Point(result[0] / 5, result[0] % 5);
+		cnt++;
+		q.add(start);
+		goal[start.r][start.c] = false;
+		while (!q.isEmpty()) {
+			Point cur = q.poll();
+			for (int i = 0; i < dc.length; i++) {
+				int nr = cur.r + dr[i];
+				int nc = cur.c + dc[i];
+				if (nr < 0 || nc < 0 || nr >= 5 || nc >= 5) {
+					continue;
+				}
+				if (goal[nr][nc]) {
+					cnt++;
+					goal[nr][nc] = false;
+					q.add(new Point(nr, nc));
+				}
+
+			}
+		}
+		if (cnt == 7) {
+			return true;
+		}
+		return false;
+	}
+
+	static boolean check() {
+		goal = new boolean[5][5];
+		int lee = 0;
+		for (int i = 0; i < result.length; i++) {
+			int tmp = result[i];
+			int r = tmp / 5;
+			int c = tmp % 5;
+			goal[r][c] = true;
+			if (map[r][c] == 'S') {
+				lee++;
+			}
+		}
+		if (lee >= 4) {
+			return true;
+		}
+		return false;
+	}
+
+	static void combi(int depth, int at) {
+		if (depth == 7) {
+//			System.out.println(Arrays.toString(result));
+			// 임도연파인지 이다솜 파인지 확인해야한다.
+			if (check()) {
+				if (bfs()) {
+					total++;
+				}
+			}
+			return;
+		}
+
+		for (int i = at; i < 25; i++) {
+			result[depth] = i;
+			combi(depth + 1, i + 1);
+		}
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		for (int i = 0; i < map.length; i++) {
-			map[i] = sc.next().toCharArray();
-		}
-		for (int i = 0; i < 25; i++) {
-			combX[i] = i % 5;
-			combY[i] = i / 5;
-		}
-		combination(new int[7], 0,0,7);
-        System.out.println(ans);
-
-	}
-
-	public static void combination(int[] comb, int idx, int depth, int left) {
-		if (left == 0) {
-			bfs(comb);
-			return;
-		}
-		if (depth == 25) {
-			return;
-		}
-		comb[idx] = depth;
-		combination(comb, idx + 1, depth + 1, left - 1);
-		combination(comb, idx, depth + 1, left);
-	}
-
-	public static void bfs(int comb[]) {
-		Queue<Integer> q = new LinkedList<>();
-		boolean visited[] = new boolean[7];
-
-		visited[0] = true;
-		q.add(comb[0]);
-		int cnt = 1, sCnt = 0;
-		while (!q.isEmpty()) {
-			int cur = q.poll();
-			if (map[combY[cur]][combX[cur]] == 'S') {
-				sCnt++;
-			}
-			for (int i = 0; i < dx.length; i++) {
-				for (int next = 1; next < 7; next++) {
-					if (!visited[next] && combX[cur] + dx[i] == combX[comb[next]]
-							&& combY[cur] + dy[i] == combY[comb[next]]) {
-						visited[next] = true;
-						q.add(comb[next]);
-						cnt++;
-					}
-				}
+		map = new char[5][5];
+		for (int i = 0; i < 5; i++) {
+			String tmp = sc.next();
+			for (int j = 0; j < 5; j++) {
+				map[i][j]=tmp.charAt(j);
 			}
 		}
-		/* 7개 모두 연결되어 있다면 */
-		if (cnt == 7) {
-			if (sCnt >= 4) {
-				ans++;
-			}
-		}
+		combi(0, 0);
+		System.out.println(total);
 	}
+
 }
