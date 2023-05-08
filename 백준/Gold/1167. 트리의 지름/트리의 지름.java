02822e139c30bf755504768db7bfbc01b15b25a3
taeyeongryu <122500557+taeyeongryu@users.noreply.github.com>
2023-05-08 16:41:09 +0900

    [Gold II] Title: 트리의 지름, Time: 1940 ms, Memory: 332248 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -0,0 +1,76 @@
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Edge {
+	int e;
+	int value;
+
+	public Edge(int e, int value) {
+		this.e = e;
+		this.value = value;
+	}
+}
+
+public class Main {
+	static boolean visited[];
+	static int[] distance;
+	static ArrayList<Edge>[] A;
+
+	private static void BFS(int index) {
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(index);
+		visited[index] = true;
+		while (!queue.isEmpty()) {
+			int now_node = queue.poll();
+			for (Edge i : A[now_node]) {
+				int e = i.e;
+				int v = i.value;
+				if (!visited[e]) {
+					visited[e] = true;
+					queue.add(e);
+					distance[e] = distance[now_node] + v;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		A = new ArrayList[N + 1];
+		for (int i = 0; i < N + 1; i++) {
+			A[i] = new ArrayList<Edge>();
+		}
+		for (int i = 0; i < N; i++) {
+			int S = sc.nextInt();
+			while (true) {
+				int E = sc.nextInt();
+				if (E == -1) {
+					break;
+				}
+				int V = sc.nextInt();
+				A[S].add(new Edge(E, V));
+			}
+		}
+		distance = new int[N + 1];
+		visited = new boolean[N + 1];
+		BFS(1);
+		int Max = 1;
+		for (int i = 2; i <= N; i++) {
+			if (distance[Max] < distance[i]) {
+				Max = i;
+			}
+		}
+		distance = new int[N + 1];
+		visited = new boolean[N + 1];
+		BFS(Max);
+		Arrays.sort(distance);
+		System.out.println(distance[N]);
+	}
+
+}
