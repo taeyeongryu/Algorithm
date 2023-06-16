630f0d77f6dc6f0a66ecc00b49d2157e751f1752
taeyeongryu <122500557+taeyeongryu@users.noreply.github.com>
2023-06-16 11:07:06 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 0.17 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,60 +1,28 @@
-import java.util.*;
-class Solution{
-    int[][] check;
-    //이 문자열에서 start부터 end까지가 펠린드롬인지 찾는것
-    boolean isPalindrome(String str,int start,int end){
-        if(check[start][end]==1){
-            return true;
-        }
-        else{
-            if(check[start][end]==-1){
-                return false;
-            }
-            //검사를 해봐야하는 것이여
-            else{
-                //여기는 check배열을 업데이트 해야되는 것이여
-                //같으면 그 아랫것들 해봐야지
-                if(str.charAt(start)==str.charAt(end)){
-                    if(start+1==end){
-                        check[start][end]=1;
-                        return true;
-                    }
-                    else{
-                        if(isPalindrome(str,start+1,end-1)){
-                            check[start][end]=1;
-                            return true;
-                        }
-                        else{
-                            check[start][end]=-1;
-                            return false;
-                        }
+class Solution
+{
+    public int solution(String s)
+    {
+        char[] chr = s.toCharArray();
+        
+        // 가장 긴 문자열부터 팰린드롬 검사
+        for (int leng = s.length(); leng > 1; leng--) {
+            
+            // 시작 인덱스
+            for (int start = 0; start + leng <= s.length(); start++) {
+                boolean chk = true;
+                
+                // 처음부터 문자열 길이의 반틈만큼 문자가 같은지 비교
+                for (int i = 0; i < leng/2; i++) {
+                    if (chr[start + i] != chr[start + leng  - i - 1]) {
+                        chk = false;
+                        break;
                     }
                 }
-                else{
-                    check[start][end]=-1;
-                    return false;
-                }
+                
+                if (chk) return leng;
             }
         }
-    }
-    public int solution(String s){
-        int answer = 0;
-        check = new int[s.length()][s.length()];
-        for(int i = 0; i<s.length() ;i++){
-            check[i][i]=1;
-        }
-        outer : for(int i = s.length(); i>=1; i--){
-            //i는 펠린드롬의 길이를 의미한다.
-            for(int j = 0; j+i-1<s.length() ;j++){
-                if(isPalindrome(s,j,j+i-1)){
-                    answer = i;
-                    break outer;
-                }
-            }
-        }
-        // System.out.println(Arrays.deepToString(check));
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        // System.out.println("Hello Java");
-        return answer;
+        
+        return 1;
     }
 }
