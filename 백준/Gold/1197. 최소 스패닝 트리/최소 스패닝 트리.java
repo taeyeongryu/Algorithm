e0ad5ed013d62311383b0ba094d50e914b702164
taeyeongryu <122500557+taeyeongryu@users.noreply.github.com>
2023-09-22 21:53:53 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 1688 ms, Memory: 272912 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,31 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
 	static int[] parent;
 	static PriorityQueue<pEdge> queue;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
 		queue = new PriorityQueue<pEdge>();
 		parent = new int[N + 1];
 		for (int i = 1; i <= N; i++) {
 			parent[i] = i;
 		}
 		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
+			int s = sc.nextInt();
+			int e = sc.nextInt();
+			int v = sc.nextInt();
 			queue.add(new pEdge(s, e, v));
 		}
-		br.close();
+		sc.close();
 		int useEdge = 0;
 		int result = 0;
 		while (useEdge < N - 1) {
