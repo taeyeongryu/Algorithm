71863870485ff006d9e64360ce7cdb7c2293e7d8
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2023-11-01 23:15:32 +0900

    [Silver II] Title: 나무 자르기, Time: 488 ms, Memory: 168156 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,56 +1,79 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        long M = Integer.parseInt(st.nextToken());
-
-        int[] treeArr = new int[N];
-
-        st = new StringTokenizer(br.readLine());
-        long minTree = 0;
-        long maxTree = 0;
-        for(int i = 0; i < treeArr.length; i++){
-            treeArr[i] = Integer.parseInt(st.nextToken());
-            if (maxTree <= treeArr[i]) {
-                maxTree = treeArr[i];
-            }
-        }
-        long result = 0;
-        long midTree = 0;
-
-
-        while(minTree<=maxTree){
-            midTree = (minTree + maxTree) / 2;
-            long total = 0;
-            for (int i = 0; i<treeArr.length; i++){
-                //칼날의 높이가 나무 높이보다 낮을 때만 나무가 잘린다.
-                if (treeArr[i] > midTree) {
-                    total += (treeArr[i] - midTree);
-                }
-            }
-            //조건을 만족
-            //칼날을 올려야 함
-            if(total>=M){
-                result = Long.max(result, midTree);
-                minTree = midTree + 1;
-            }
-            //조건을 만족하지 않음
-            //칼날을 내려야 함
-            else{
-                maxTree=midTree-1;
-            }
-        }
-        bw.append(result+"");
-        bw.close();
-        br.close();
-    }
+	static int n,m;
+	static int[] tree;
+	static int target,start,end;
+	static int answer;
+	static int now_h;
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		tree = new int[n+1];
+		st = new StringTokenizer(br.readLine());
+		int max=0;
+		for(int i=0;i<n;i++) {
+			tree[i]= Integer.parseInt(st.nextToken());
+			if (max<tree[i]) {
+				max=tree[i];
+			}
+		}
+		
+		// 이분 탐색 - binary search
+		start = 0;
+		end = max;
+		
+		answer = binary_search(start, end);
+		System.out.println(answer);
+		
+	}
+	private static int binary_search(int start, int end){
+		if (start>end) {
+//			System.out.println("실패!!");
+			return now_h;
+//			return 0;
+		}
+		int mid = (start + end)/2;
+		long total=0;
+//		System.out.println("현재 절단기 높이는 "+mid);
+		for(int i=0;i<n;i++) {
+			
+			int tmp = tree[i]-mid;
+	
+//			System.out.println("절단중... "+tmp+"획득 ");
+			if(tmp>0) {
+				total+=tmp;
+			}
+		}
+		
+//		System.out.println("필요 나무 : "+m);
+//		System.out.println("절단한 나무 : "+total);
+		//지금은 낭비  ! 
+		if(total > m) {
+//			System.out.println("낭비 입니다 절단기 높히세요 !!");
+			now_h=mid;
+//			System.out.println(now_h);
+			binary_search(mid+1,end);
+		}
+		else if(total == m) {
+			now_h=mid;
+			return now_h;
+		}
+		
+		//지금은 낭비 아님 - 더깎아!
+		else if(total<m) {
+//			System.out.println("아직 나무 부족! 절단기 낮추세요 !!");
+			
+			binary_search(start,mid-1);
+		}
+		
+		return now_h;
+	}
 }
