684ab5b79cc9586d1a99b15be8c653f87a0cb2e5
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2023-12-11 12:05:08 +0900

    [Bronze V] Title: 문자와 문자열, Time: 88 ms, Memory: 11564 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String inputStr = br.readLine();
+        int inputInt = Integer.parseInt(br.readLine());
+
+        bw.append(inputStr.charAt(inputInt - 1));
+        bw.close();
+        br.close();
+    }
+}
