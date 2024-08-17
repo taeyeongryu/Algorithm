0d7a8b219726303454a02c50743f43f6ebfe7bab
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-08-17 15:06:51 +0900

    [Silver V] Title: 나이순 정렬, Time: 532 ms, Memory: 55196 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -2,45 +2,45 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static class Person implements Comparable<Person> {
+    static class Person{
         int age;
         String name;
-
-        private Person(int age, String name) {
+        public Person(int age, String name){
             this.age = age;
             this.name = name;
         }
         @Override
-        public int compareTo(Person person) {
-            return Integer.compare(this.age, person.age);
+        public String toString(){
+            return "age : " + age + ", name : " + name;
         }
     }
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
+
         List<Person> list = new ArrayList<>();
-        StringTokenizer st = null;
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            list.add(new Person(Integer.parseInt(st.nextToken()), st.nextToken()));
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+            list.add(new Person(age, name));
         }
-
-        Collections.sort(list);
-
-        for(Person person : list){
-            bw.append("" + person.age + " " + person.name + "\n");
+        Collections.sort(list, new Comparator<Person>(){
+            @Override
+            public int compare(Person p1, Person p2){
+                return Integer.compare(p1.age, p2.age);
+            }
+        });
+        for(Person p : list){
+            bw.append(p.age + " " + p.name + "\n");
         }
         bw.close();
         br.close();
-
     }
 }
