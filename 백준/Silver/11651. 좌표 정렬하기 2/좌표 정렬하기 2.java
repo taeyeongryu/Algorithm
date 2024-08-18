49ad7440c6193e211153910c6de2430bd11d4171
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-08-18 11:50:29 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 596 ms, Memory: 59324 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -2,49 +2,44 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static class Point implements Comparable<Point>{
+    static class Point{
         int x;
         int y;
-        private Point(){
-        }
-        private Point(int x, int y){
+        Point(int x, int y){
             this.x = x;
             this.y = y;
         }
         @Override
-        public int compareTo(Point p){
-            if (Integer.compare(this.y, p.y) != 0) {
-                return Integer.compare(this.y, p.y);
-            }else{
-                return Integer.compare(this.x, p.x);
-            }
+        public String toString(){
+            return "x : " + x + ", y : " + y;
         }
+
     }
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-
-        StringTokenizer st = null;
         List<Point> list = new ArrayList<>();
 
+        StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
 
-            list.add(new Point(x, y));
+            list.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
-        Collections.sort(list);
+        Collections.sort(list,new Comparator<Point>(){
+            @Override
+            public int compare(Point p1, Point p2){
+                return Integer.compare(p1.y, p2.y) == 0 ? Integer.compare(p1.x, p2.x) : Integer.compare(p1.y, p2.y);
+            }
+        });
         for (Point point : list) {
-            bw.append("" + point.x + " " + point.y + "\n");
+            bw.append(point.x + " " + point.y + "\n");
         }
         bw.close();
         br.close();
