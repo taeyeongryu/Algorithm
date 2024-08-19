5a9d6b154a7120b1840c2089b205ee083b90f7cf
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-08-19 22:04:32 +0900

    [Silver IV] Title: 카드2, Time: 152 ms, Memory: 52276 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            q.offer(i);
+        }
+        while (q.size() != 1) {
+            q.poll();
+            int num = q.poll();
+            q.offer(num);
+        }
+        System.out.println(q.poll());
+    }
+}
