f5ad5d98de231a6f11bc06d5ff80d7fbffafb9cb
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-08-20 21:22:00 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 120 ms, Memory: 26408 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -9,41 +9,40 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Queue<Integer> queue = new LinkedList<>();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= n; i++) {
-            queue.offer(i);
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            q.offer(i);
         }
-        int count = 1;
-
-        List<Integer> result = new ArrayList<>();
-
-        while(!queue.isEmpty()){
-            if(count==k){
-                count = 1;
-                Integer poll = queue.poll();
-                result.add(poll);
-            }
-            else{
-                count++;
-                queue.offer(queue.poll());
+        int count = 0;
+        List<Integer> list = new ArrayList<>();
+        while(!q.isEmpty()){
+            count++;
+            int poll = q.poll();
+            if(count!=K){
+                q.offer(poll);
+            }else{
+                list.add(poll);
+                count = 0;
             }
         }
         bw.append("<");
-        for (int i = 0; i < result.size(); i++) {
-            if(i!=result.size()-1){
-                bw.append(result.get(i) + ", ");
-            }else{
-                bw.append(result.get(i)+"");
+
+        for (int i = 0; i < list.size(); i++) {
+            bw.append(list.get(i)+"");
+            if(i==list.size()-1){
+                break;
             }
+            bw.append(", ");
         }
         bw.append(">");
         bw.close();
         br.close();
+
     }
 }
