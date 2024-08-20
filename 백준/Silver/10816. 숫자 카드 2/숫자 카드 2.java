525e12772bafd6f1324a592180cf776827b8e2d5
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-08-20 20:57:03 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1120 ms, Memory: 271836 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,63 +1,37 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.OutputStreamWriter;
+import java.util.*;
 
 public class Main {
-	static List<Integer> list = new ArrayList<>();
-	static int N, M;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	static int lower(int target) {
-		int start = 0;
-		int end = list.size();
-		while (start < end) {
-			int mid = (start + end) / 2;
-			if (list.get(mid) >= target) {
-				end = mid;
-			} else {
-				start = mid + 1;
-			}
-		}
-		return start;
-	}
+        int N = Integer.parseInt(br.readLine());
 
-	static int upper(int target) {
-		int start = 0;
-		int end = list.size();
-		while (start < end) {
-			int mid = (start + end) / 2;
-			if (list.get(mid) <= target) {
-				start = mid + 1;
-			} else {
-				end = mid;
-			}
-		}
-		return start;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Map<Integer,Integer> map = new HashMap<>();
 
-	}
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            map.put(input, map.getOrDefault(input, 0) + 1);
+        }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			list.add(Integer.parseInt(st.nextToken()));
-		}
-		Collections.sort(list);
-		M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < M; i++) {
-			int tmp = Integer.parseInt(st.nextToken());
-			int start = lower(tmp);
-			int end = upper(tmp);
-			sb.append(end-start+" ");
-		}
-		System.out.println(sb.toString());
+        int M = Integer.parseInt(br.readLine());
 
-	}
+        st = new StringTokenizer(br.readLine());
 
+        for (int i = 0; i < M; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            if (map.containsKey(input)) {
+                bw.append(map.get(input) + " ");
+            }else{
+                bw.append("0 ");
+            }
+        }
+        bw.close();
+        br.close();
+    }
 }
