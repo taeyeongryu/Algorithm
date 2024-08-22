84663010116a51a2993968076e0d60496ba61c41
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-08-22 20:15:57 +0900

    [Silver II] Title: 랜선 자르기, Time: 124 ms, Memory: 15600 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -2,48 +2,49 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int K, N;
-    static long MAX = Long.MIN_VALUE;
-    static int[] kable;
-    static void maxSize(){
-        long left = 1;
-        long right = Integer.MAX_VALUE;
-        long mid = 0;
-        while(left<=right){
-            mid = (left + right) / 2;
-            long total = count(mid);
-            if(total>=N){
-                MAX = Math.max(MAX, mid);
-                left = mid + 1;
-            }
-            else{
-                right = mid - 1;
-            }
-        }
-    }
-    private static long count(long mid) {
-        long count = 0;
-        for (int i : kable) {
-            count += i / mid;
-        }
-        return count;
-    }
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        K = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        kable = new int[K];
-        for (int i = 0; i < K; i++) {
-            kable[i] = Integer.parseInt(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        long[] arr = new long[K];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        maxSize();
-        System.out.println(MAX);
+        Arrays.sort(arr);
+        long min = 1;
+        long max = arr[K - 1];
+
+
+        long result = Integer.MIN_VALUE;
+
+        while(min<=max){
+            long mid = (min + max) / 2;
+            long count = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                count += arr[i] / mid;
+            }
+            if(count>=N){
+                result = Math.max(mid, result);
+                min = mid + 1;
+            }else{
+                max = mid-1;
+            }
         }
+        bw.append(result + "");
+        bw.close();
+        br.close();
+
+    }
 }
