44dcdceec60355a4bde0be6bed4d79d88d2df473
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-08-25 21:43:04 +0900

    [Silver II] Title: 유기농 배추, Time: 100 ms, Memory: 13660 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.LinkedList;
@@ -8,82 +7,70 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Point {
-		int r;
-		int c;
+    static boolean[][] map;
+    static boolean[][] visited;
+    static int[] dr = {1, -1, 0, 0};
+    static int[] dc = {0, 0, 1, -1,};
+    static Queue<int[]> queue;
+    static int M,N;
+    static void bfs() {
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
 
-		@Override
-		public String toString() {
-			return "Point [r=" + r + ", c=" + c + "]";
-		}
+            for (int i = 0; i < 4; i++) {
+                int nextR = cur[0]+dr[i];
+                int nextC = cur[1]+dc[i];
 
-		public Point(int r, int c) {
-			super();
-			this.r = r;
-			this.c = c;
-		}
+                if (nextR < 0 || nextR >= N || nextC < 0 || nextC >= M) {
+                    continue;
+                }
 
-	}
+                if (!visited[nextR][nextC] && map[nextR][nextC]) {
+                    visited[nextR][nextC] = true;
+                    queue.offer(new int[]{nextR, nextC});
+                }
+            }
+        }
 
-	static int N, M, K, result;
-	static int[][] map;
-	static boolean[][] visited;
-	static int[] dr = { 1, -1, 0, 0 };
-	static int[] dc = { 0, 0, 1, -1 };
-	static Queue<Point> q = new LinkedList<>();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 0; tc < T; tc++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			M = Integer.parseInt(st.nextToken());
-			N = Integer.parseInt(st.nextToken());
-			K = Integer.parseInt(st.nextToken());
-			map = new int[N][M];
-			visited = new boolean[N][M];
-			for (int i = 0; i < K; i++) {
-				st = new StringTokenizer(br.readLine());
-				int c = Integer.parseInt(st.nextToken());
-				int r = Integer.parseInt(st.nextToken());
-				map[r][c] = 1;
-			}
-			result = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					if (map[i][j] == 1 && !visited[i][j]) {
-						visited[i][j] = true;
-						result++;
-						q.add(new Point(i, j));
-						bfs();
-					}
-				}
-			}
-			bw.append(result + "\n");
-		}
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
 
-		bw.close();
-		br.close();
-	}
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
 
-	private static void bfs() {
-		while (!q.isEmpty()) {
-			Point cur = q.poll();
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            map = new boolean[N][M];
+            visited = new boolean[N][M];
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int C = Integer.parseInt(st.nextToken());
+                int R = Integer.parseInt(st.nextToken());
+                map[R][C]=true;
+            }
+            int result = 0;
 
-			for (int i = 0; i < dc.length; i++) {
-				int nr = cur.r + dr[i];
-				int nc = cur.c + dc[i];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if(map[i][j]&&!visited[i][j]){
+                        visited[i][j]=true;
+                        queue = new LinkedList<>();
+                        queue.offer(new int[]{i, j});
+                        result++;
+                        bfs();
+                    }
+                }
+            }
 
-				if (nr < 0 || nc < 0 || nr >= N || nc >= M) {
-					continue;
-				}
-				if (!visited[nr][nc] && map[nr][nc] == 1) {
-					visited[nr][nc] = true;
-					q.add(new Point(nr, nc));
-				}
-			}
-		}
-	}
-
+            bw.append(result + "\n");
+        }
+        bw.close();
+        br.close();
+    }
 }
