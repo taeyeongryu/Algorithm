23e513e30c82450a0df22154d2977d91e2ae51c5
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-08-25 12:43:23 +0900

    [Silver III] Title: 1로 만들기, Time: 108 ms, Memory: 16008 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,33 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] arr = new int[N + 1];
-		arr[1] = 0;
-		for (int i = 2; i < arr.length; i++) {
-			if (i % 3 == 0) {
-				arr[i] = arr[i / 3] + 1;
-			}
-			if (i % 2 == 0) {
-				if (arr[i] != 0) {
-					arr[i] = Math.min(arr[i], arr[i / 2] + 1);
-				} else {
-					arr[i] = arr[i / 2] + 1;
+        int N = Integer.parseInt(br.readLine());
 
-				}
-			}
-			if (arr[i] != 0) {
-				arr[i] = Math.min(arr[i], arr[i - 1] + 1);
-			} else {
-				arr[i] = arr[i - 1] + 1;
-			}
-		}
-		System.out.println(arr[N]);
-
-	}
-
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        dp[1] = 0;
+        if(N>=2){
+            for (int i = 2; i < dp.length; i++) {
+                if (i % 3 == 0) {
+                    dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+                }
+                if (i % 2 == 0) {
+                    dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+                }
+                dp[i] = Math.min(dp[i], dp[i - 1] + 1);
+        }
+        }
+//        System.out.println("Arrays.toString(dp) = " + Arrays.toString(dp));
+        bw.append(dp[N]+"");
+        bw.close();
+        br.close();
+    }
 }
