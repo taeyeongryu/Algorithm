e374af00c47ba6c08fd2bf6dc327cd0622d6893b
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-08-25 15:35:16 +0900

    [Silver III] Title: 파도반 수열, Time: 68 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,20 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		long[] F = new long[101];
-		F[1]=1;
-		F[2]=1;
-		F[3]=1;
-		for (int i = 4; i < F.length; i++) {
-			F[i]=F[i-2]+F[i-3];
-		}
-		for (int i = 0; i < T; i++) {
-			int N = sc.nextInt();
-			System.out.println(F[N]);
-		}
-	}
+        long[] arr = new long[101];
+        arr[1] = 1;
+        arr[2] = 1;
+        arr[3] = 1;
+        arr[4] = 2;
+        arr[5] = 2;
+        for (int i = 6; i < arr.length; i++) {
+            arr[i] = arr[i - 1] + arr[i - 5];
+        }
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            bw.append(arr[N] + "\n");
+        }
+        bw.close();
+        br.close();
+    }
 }
