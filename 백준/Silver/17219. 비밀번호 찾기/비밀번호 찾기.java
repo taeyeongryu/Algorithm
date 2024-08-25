fd1a470bd7dabfe97ecf0809659afba47d6bc7a5
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-08-25 11:55:59 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 508 ms, Memory: 75608 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -1,31 +1,30 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
+
 
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
+        StringTokenizer st= new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        Map<String, String> map = new HashMap<>();
 
+        Map<String, String> map = new HashMap<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            map.put(st.nextToken(), st.nextToken());
+            String address = st.nextToken();
+            String password = st.nextToken();
+            map.put(address, password);
         }
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            bw.append(map.get(st.nextToken()) + "\n");
+            String address = br.readLine();
+            bw.append(map.get(address) + "\n");
         }
         bw.close();
         br.close();
+
     }
 }
