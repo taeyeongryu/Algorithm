4cc5e0f0b3ffdc48162141964b6770fff25138e8
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-08-29 20:23:38 +0900

    [Silver II] Title: 마인크래프트, Time: 480 ms, Memory: 35820 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -2,72 +2,54 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, B,min,max,minTime=Integer.MAX_VALUE,minHeight;
-    static int[][] map;
 
-    static void goToHeight(int height) {
-        int time=0;
-        int block = 0;
-        boolean flag = true;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                //블록을 쌓는 경우
-                if (map[i][j] < height) {
-                    //블록이 존재하면
-                    time += (height - map[i][j]);
-
-                    block += (height - map[i][j]);
-                }
-                //블록을 제거하는 경우
-                else if (map[i][j] > height) {
-                    time += (map[i][j] - height) * 2;
-                    block -= (map[i][j] - height);
-                }
-            }
-        }
-        if(block>B){
-            flag = false;
-        }
-        if (flag) {
-            if(minTime >= time){
-                minTime = time;
-                minHeight = height;
-            }
-        }
-    }
-
+    static int N, M, B, sum,min,max;
+    static int[] heights;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
+        heights = new int[N * M];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                min = Math.min(min, map[i][j]);
-                max = Math.max(max, map[i][j]);
+                heights[i * M + j] = Integer.parseInt(st.nextToken());
+                sum += heights[i * M + j];
             }
         }
-        if(min!=max){
-            for (int height = min; height <=max ; height++) {
-                goToHeight(height);
+        Arrays.sort(heights);
+        min = heights[0];
+        max =  (B + sum) / (N * M);
+        
+        
+        long result = Long.MAX_VALUE;
+        int height = 0;
+        for (int i = min; i <= max; i++) {
+            long tmp = 0;
+            for (int j = 0; j < heights.length; j++) {
+                if(heights[j]<=i){
+                    tmp += i - heights[j];
+                }else{
+                    tmp += (heights[j] - i) * 2;
+                }
             }
-        }else{
-            minTime = 0;
-            minHeight = max;
+            if(result>=tmp){
+                result =tmp;
+                height = i;
+            }
+
         }
-        bw.append(minTime + " " + minHeight);
+        bw.append(result + " " + height);
         bw.close();
         br.close();
     }
