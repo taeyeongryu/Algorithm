bfac9d2451a43a884ac7c4e3edba1d2aa4a78c22
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-08-29 11:01:02 +0900

    [Silver II] Title: 최대 힙, Time: 264 ms, Memory: 26896 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class Main {
@@ -10,18 +11,18 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for (int i = 0; i < N; i++) {
             int input = Integer.parseInt(br.readLine());
-            if(input==0){
+            if(input>0){
+                pq.offer(input);
+            }else{
                 if(pq.isEmpty()){
                     bw.append("0\n");
+                }else{
+                    bw.append(pq.poll()+"\n");
                 }
-                else{
-                    bw.append(-pq.poll()+"\n");
-                }
-            }else{
-                pq.offer(-input);
             }
         }
         bw.close();
