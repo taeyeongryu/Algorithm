f18b3d88be434eba8f7a22fbf829eea3e24271b5
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-09-01 14:27:01 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 592 ms, Memory: 76116 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -7,53 +7,30 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N, M;
+    static int[] start;
+    static int[][] map;
+    static int[][] visited;
     static int[] dr = {1, -1, 0, 0};
-    static int[] dc = {0, 0, 1, -1};
-    static class Node{
-        int r;
-        int c;
-        int dist;
-        public Node(int r, int c, int dist) {
-            this.r = r;
-            this.c = c;
-            this.dist = dist;
-        }
-    }
-    static int[][] map,dist;
-    static boolean[][] visited;
-    static int N,M,startR,startC;
-
+    static int[] dc = {0, 0, -1, 1};
     static void bfs(){
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(startR, startC, 0));
+        Queue<int[]> queue = new LinkedList<>();
+        visited[start[0]][start[1]] = 0;
+        queue.offer(start);
 
-        while(!q.isEmpty()){
-            Node cur = q.poll();
-            int r = cur.r;
-            int c = cur.c;
-            int curDist = cur.dist;
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                //범위 check
-                if (nr < 0 || nr >= N || nc < 0 || nc >= M) {
+                int nextR = cur[0]+dr[i];
+                int nextC = cur[1]+dc[i];
+
+                if (nextR < 0 || nextR >= N || nextC < 0 || nextC >= M) {
                     continue;
                 }
-                //map위치 1인지 2인지 0인지 check
-                if (map[nr][nc] == 1 && !visited[nr][nc]) {
-                    visited[nr][nc] = true;
-                    dist[nr][nc]=curDist+1;
-                    q.offer(new Node(nr, nc, curDist + 1));
-                }
-            }
-        }
-    }
-    static void noVisitCheck(){
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 1 && !visited[i][j]) {
-                    dist[i][j] = -1;
+                if (visited[nextR][nextC] == 0 && map[nextR][nextC] == 1) {
+                    visited[nextR][nextC] = visited[cur[0]][cur[1]] + 1;
+                    queue.offer(new int[]{nextR, nextC});
                 }
             }
         }
@@ -62,34 +39,35 @@ public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         map = new int[N][M];
-        dist = new int[N][M];
-        visited = new boolean[N][M];
+        visited = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j]==2){
-                    startR = i;
-                    startC = j;
+                if (map[i][j] == 2) {
+                    start = new int[]{i, j};
                 }
             }
         }
         bfs();
-        noVisitCheck();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                bw.append(dist[i][j] + " ");
+                if (map[i][j] == 1 && visited[i][j] == 0) {
+                    visited[i][j] = -1;
+                }
+                bw.append(visited[i][j] + " ");
             }
             bw.append("\n");
         }
         bw.close();
         br.close();
-
     }
 }
