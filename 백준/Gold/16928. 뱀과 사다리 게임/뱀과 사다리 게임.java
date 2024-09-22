09777134c228ef081fd14930507fdce98190e8bc
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-09-22 21:06:06 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 68 ms, Memory: 11768 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -7,90 +7,82 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Node{
-        int index;
-        int time;
+   static class Node{
+       int n;
+       int time;
+       public Node(int n, int time) {
+           this.n = n;
+           this.time = time;
+       }
+       @Override
+       public String toString(){
+           return "n : " + n + ", time : " + time;
+       }
+   }
+   static int[] teleport;
+   static boolean[] visited;
+   static int N,M;
+   static Queue<Node> queue;
+   static int findRealGoal(int n){
+       if(teleport[n]==0){
+           return n;
+       }else{
+           return findRealGoal(teleport[n]);
+       }
+   }
+   static int bfs(){
+       int result = 0;
+       while(!queue.isEmpty()){
 
-        public Node(int index, int time) {
-            this.index = index;
-            this.time = time;
-        }
+           Node cur = queue.poll();
+           int curN = cur.n;
+           int curTime = cur.time;
 
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "index=" + index +
-                    ", time=" + time +
-                    '}';
-        }
-    }
-    static int[] map = new int[101];
-    static boolean[] visited = new boolean[101];
-    static int[] ladder = new int[101];
-    static int[] snake = new int[101];
+           if(curN==100){
+               result = curTime;
+               break;
+           }
 
-    static int bfs(){
-        int result = 0;
-        Queue<Node> q = new LinkedList<>();
-        visited[1] = true;
-        q.offer(new Node(1, 0));
-
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-            int index = cur.index;
-            int time = cur.time;
-
-            if (index == 100) {
-                result = time;
-                break;
-            }
-            for (int i = 6; i >= 1; i--) {
-                //다음 위치
-                int nextIndex = index + i;
-                //위치가 벗어나지 않는지 check
-                if (!(1 <= nextIndex && nextIndex <= 100)) {
-                    continue;
-                }
-                //사다리 라면
-                if(ladder[nextIndex]!=0) {
-                    //위치 이동
-                    nextIndex = ladder[nextIndex];
-                }
-                //뱀이라면
-                else if (snake[nextIndex]!=0){
-                    //위치 이동
-                    nextIndex = snake[nextIndex];
-                }
-                //방문한적 있는지 check
-                if(visited[nextIndex]){
-                    continue;
-                }
-                visited[nextIndex] = true;
-                q.offer(new Node(nextIndex, time + 1));
-            }
-        }
-        return result;
-    }
-
+           for (int i = 1; i <= 6; i++) {
+               int nextN = curN + i;
+               if (nextN > 100) {
+                   continue;
+               }
+               int realGoal = findRealGoal(nextN);
+               if(!visited[realGoal]){
+                   visited[realGoal]=true;
+                   queue.offer(new Node(realGoal, curTime + 1));
+               }
+           }
+       }
+       return result;
+   }
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        teleport = new int[101];
+        visited = new boolean[101];
+        queue = new LinkedList<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            ladder[start] = end;
+            teleport[start] = end;
         }
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
+            int start =Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            snake[start] = end;
+            teleport[start] = end;
         }
-        bw.append("" + bfs());
+        visited[1]=true;
+        queue.offer(new Node(1, 0));
+        bw.append(bfs() + "");
         bw.close();
         br.close();
     }
