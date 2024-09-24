1a5fb75fa085a4a8aab40b8422e1dcb15ef8518e
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-09-24 21:30:35 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2976 ms, Memory: 454348 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -5,103 +5,106 @@ import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
-    static PriorityQueue<Integer> maxQueue= new PriorityQueue<>(Collections.reverseOrder());
-    static PriorityQueue<Integer> minQueue= new PriorityQueue<>();
-    static Map<Integer, Integer> map = new HashMap<>();
+    static PriorityQueue<Integer> maxQueue;
+    static PriorityQueue<Integer> minQueue;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
-        StringTokenizer st = null;
-        for (int i = 0; i < T; i++) {
-            maxQueue.clear();
-            minQueue.clear();
-            map.clear();
-            int k = Integer.parseInt(br.readLine());
-            for (int j = 0; j < k; j++) {
-                st = new StringTokenizer(br.readLine());
-                String oper = st.nextToken();
-                int num = Integer.parseInt(st.nextToken());
-                //값 삽입
-                if (oper.equals("I")) {
-                    insert(num);
-                } else {
-                    //최대값 제거
-                    if (num==1){
-                        removeMaxValue();
-                    }
-                    //최소값 제거
-                    else{
-                        removeMinValue();
-                    }
-                }
-            }
+        StringTokenizer st;
+        Map<Integer, Integer> map;
 
-            if (!map.isEmpty()) {
-                int max = removeMaxValue();
-                bw.append(max+ " ");
-                if(map.isEmpty()){
-                    bw.append(max+"\n");    
-                }else{
-                    bw.append(removeMinValue() + "\n");
+        for (int t = 0; t < T; t++) {
+            map = new HashMap<>();
+            minQueue = new PriorityQueue<>();
+            maxQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+
+            int K = Integer.parseInt(br.readLine());
+            for (int i = 0; i < K; i++) {
+
+                st = new StringTokenizer(br.readLine());
+                char operator = st.nextToken().charAt(0);
+                int inputNum = Integer.parseInt(st.nextToken());
+                //삽입
+                if(operator=='I'){
+                    map.put(inputNum, map.getOrDefault(inputNum, 0) + 1);
+                    maxQueue.offer(inputNum);
+                    minQueue.offer(inputNum);
                 }
-            } else {
+                //출력
+                else{
+                    //최대값 빼야한다면
+                    if (inputNum == 1) {
+                        while (!maxQueue.isEmpty()) {
+                            int peek = maxQueue.peek();
+                            int num = map.getOrDefault(peek, 0);
+                            if (num > 0) {
+                                break;
+                            } else {
+                                maxQueue.poll();
+                            }
+                        }
+                        if(!maxQueue.isEmpty()){
+                            int peek = maxQueue.peek();
+                            int stock = map.getOrDefault(peek, 0);
+                            if (stock > 1) {
+                                map.put(peek, map.get(peek) - 1);
+                            } else if (stock == 1) {
+                                map.remove(peek);
+                            }
+                            //없으면 그냥 지나가 ~
+                            maxQueue.poll();
+                        }
+                    }
+                    //최소값 빼기
+                    else {
+                        while (!minQueue.isEmpty()) {
+                            int peek = minQueue.peek();
+                            int num = map.getOrDefault(peek, 0);
+                            if (num > 0) {
+                                break;
+                            } else {
+                                minQueue.poll();
+                            }
+                        }
+                        if(!minQueue.isEmpty()){
+                            int peek = minQueue.peek();
+                            int stock = map.getOrDefault(peek, 0);
+                            if (stock > 1) {
+                                map.put(peek, map.get(peek) - 1);
+                            } else if (stock == 1) {
+                                map.remove(peek);
+                            }
+                            //없으면 그냥 지나가 ~
+                            minQueue.poll();
+                        }
+                    }
+                }
+
+            }
+            if (map.isEmpty()) {
                 bw.append("EMPTY\n");
+            } else  {
+                while(!maxQueue.isEmpty()){
+                    int poll = maxQueue.poll();
+                    if(map.containsKey(poll)){
+                        bw.append(poll + " ");
+                        break;
+                    }
+                }
+                while(!minQueue.isEmpty()){
+                    int poll = minQueue.poll();
+                    if(map.containsKey(poll)){
+                        bw.append(poll + "\n");
+                        break;
+                    }
+                }
             }
         }
         bw.close();
         br.close();
-    }
-
-    //현재 queue에서 가지고있는 최소값을 제거한다.
-    //맞지 않는 최소값도 계속 삭제한다.
-    private static int  removeMinValue() {
-        int min = 0;
-        if(!map.isEmpty()){
-            while(true){
-                min = minQueue.poll();
-                int getValue = map.getOrDefault(min, 0);
-                if(getValue ==0){
-                    continue;
-                } else if (getValue ==1) {
-                    map.remove(min);
-                    break;
-                }else{
-                    map.put(min, getValue - 1);
-                    break;
-                }
-            }
-
-        }
-        return min;
-    }
-
-    private static int  removeMaxValue() {
-        int max = 0;
-        if(!map.isEmpty()){
-            while(true){
-                max = maxQueue.poll();
-                int getValue = map.getOrDefault(max, 0);
-                if(getValue ==0){
-                    continue;
-                } else if (getValue ==1) {
-                    map.remove(max);
-                    break;
-                }else{
-                    map.put(max, getValue - 1);
-                    break;
-                }
-            }
-
-        }
-        return max;
-    }
-
-    private static void insert(int num) {
-        maxQueue.offer(num);
-        minQueue.offer(num);
-        map.put(num, map.getOrDefault(num, 0) + 1);
     }
 }
