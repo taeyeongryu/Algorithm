7806d54ccc6dae3bea3555d507cebe08c5911b98
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-09-24 22:32:03 +0900

    [Gold IV] Title: DSLR, Time: 3640 ms, Memory: 296812 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -8,102 +8,69 @@ import java.util.StringTokenizer;
 
 public class Main {
     static class Node{
-        int num;
-        String operations;
-
-        public Node(int num, String operations) {
-            this.num = num;
-            this.operations = operations;
+        int index;
+        String oper;
+        public Node(int index, String oper){
+            this.index = index;
+            this.oper = oper;
         }
     }
-
+    public static int D(int input){
+        return (input * 2) % 10000;
+    }
+    public static int S(int input){
+        return input - 1 == -1 ? 9999 : input - 1;
+    }
+    public static int L(int input){
+        int q = input / 1000;
+        int r = input % 1000;
+        return r * 10 + q;
+    }
+    public static int R (int input){
+        int q = input / 10;
+        int r = input % 10;
+        return r * 1000 + q;
+    }
     static boolean[] visited;
+    static Queue<Node> queue;
     static String bfs(int start, int end){
-        Queue<Node> q = new LinkedList<>();
-        visited[start]=true;
-        q.offer(new Node(start, ""));
         String result = "";
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-            int num = cur.num;
-            String operations = cur.operations;
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            int curIndex = cur.index;
+            String curOper = cur.oper;
 
-            if(num==end){
-                result = operations;
+            if(curIndex==end){
+                result = curOper;
                 break;
             }
-            int numD = D(num);
-            if (!visited[numD]){
-                visited[numD]=true;
-                String nOperations = operations + "D";
-                q.offer(new Node(numD, nOperations));
+
+            int nextD = D(curIndex);
+            int nextS = S(curIndex);
+            int nextL = L(curIndex);
+            int nextR = R(curIndex);
+          
+            if (!visited[nextD]) {
+                visited[nextD] = true;
+                queue.offer(new Node(nextD, curOper + "D"));
             }
-            int numS = S(num);
-            if (!visited[numS]){
-                visited[numS]=true;
-                String nOperations = operations + "S";
-                q.offer(new Node(numS, nOperations));
+
+            if (!visited[nextS]) {
+                visited[nextS] = true;
+                queue.offer(new Node(nextS, curOper + "S"));
             }
-            int numL = L(num);
-            if (!visited[numL]){
-                visited[numL]=true;
-                String nOperations = operations + "L";
-                q.offer(new Node(numL, nOperations));
+
+            if (!visited[nextL]) {
+                visited[nextL] = true;
+                queue.offer(new Node(nextL, curOper + "L"));
             }
-            int numR = R(num);
-            if (!visited[numR]){
-                visited[numR]=true;
-                String nOperations = operations + "R";
-                q.offer(new Node(numR, nOperations));
+
+            if (!visited[nextR]) {
+                visited[nextR] = true;
+                queue.offer(new Node(nextR, curOper + "R"));
             }
         }
         return result;
-    }
-    private static int[] intToArr(int input) {
-        int[] arr = new int[4];
-        if(input==0){
-            return arr;
-        }
-        if (1 <= input) {
-            arr[3] = input % 10;
-        }
-        if (10 <= input) {
-            arr[2] = (input / 10) % 10;}
-        if (100 <= input) {
-            arr[1] = (input / 100) % 10 ;
-        }
-        if (1000 <= input) {
-            arr[0] = (input / 1000) % 10;
-        }
-        return arr;
-    }
-    static int D(int input){
-        int num = input * 2;
-        return num % 10000;
-    }
-
-    static int S(int input){
-        int num = input - 1;
-        if (num<0) return 9999;
-        return num;
-    }
-    static int L(int input){
-        int[] numArr = intToArr(input);
-        int[] returnArr = new int[4];
-        for (int i = 1; i <=3 ; i++) {
-            returnArr[i - 1] = numArr[i];
-        }
-        returnArr[3] = numArr[0];
-        return 1000 * returnArr[0] + 100 * returnArr[1] + 10 * returnArr[2] + returnArr[3];
-    }
-    static int R(int input){
-        int[] numArr = intToArr(input);
-        int[] returnArr = new int[4];
-        for (int i = 0; i <=2 ; i++) {
-            returnArr[i + 1] = numArr[i];
-        }
-        returnArr[0] = numArr[3];
-        return 1000 * returnArr[0] + 100 * returnArr[1] + 10 * returnArr[2] + returnArr[3];
     }
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -113,11 +80,14 @@ public class Main {
 
         StringTokenizer st = null;
 
-        for (int i = 0; i < T; i++) {
+        for (int t = 0; t < T; t++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             visited = new boolean[10000];
+            queue = new LinkedList<>();
+            visited[start]=true;
+            queue.offer(new Node(start, ""));
             String result = bfs(start, end);
             bw.append(result + "\n");
         }
