859e9530a74771bea100e4dda590e87a6dff6b2d
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-09-26 22:45:07 +0900

    [Silver III] Title: N과 M (4), Time: 108 ms, Memory: 21808 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,36 +1,36 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static int[] result;
-	static StringBuilder sb = new StringBuilder();
+    static int N,M;
+    static int[] arr;
+    public static StringBuilder sb;
 
-	static void Back(int depth, int at) {
-		if (depth == M) {
-			for (int i = 0; i < result.length; i++) {
-				sb.append("" + result[i] + " ");
-			}
-			sb.append("\n");
-			return;
-		}
-		for (int i = at; i <= N; i++) {
-			result[depth] = i;
-			Back(depth + 1, i);
-		}
-	}
+    public static void dfs(int depth, int at){
+        if (depth == M) {
+            for (int i = 0; i < arr.length; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = at; i < N + 1; i++) {
+            arr[depth] = i;
+            dfs(depth + 1, i);
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		// N,M주어진다. N개중에서 순서를 고려해서 M개 뽑는 것이다.중복은 허용안됨
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		result = new int[M];
-		Back(0, 1);
-		System.out.println(sb.toString());
-	}
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        dfs(0, 1);
+        System.out.println(sb.toString());
+    }
 }
