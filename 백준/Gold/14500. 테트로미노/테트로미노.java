8784f29ec7add9f836f532055bc0332fccad5654
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-09-26 22:10:38 +0900

    [Gold IV] Title: 테트로미노, Time: 552 ms, Memory: 33196 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,175 +1,94 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	// 시계방향
-	// 상 우 하 좌
-	// 0 1 2 3
-	static int[] dr = { -1, 0, 1, 0 };
-	static int[] dc = { 0, 1, 0, -1 };
-	static int N;
-	static int M;
-	static int[][] map;
+    static int N, M;
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
+    static int[][] map;
+    static boolean[][] visited;
+    static int max;
+    //map 영역인지는 확인하고 들어온다, 즉 자리는 있는것
+    //방문했는지도 확인함
+    static void dfs(int r, int c, int sum, int depth) {
+//        System.out.println("r : " + r + ", c : " + c);
+        int num = map[r][c];
+        if (depth == 3) {
+            max = Math.max(max, sum + num);
+            return;
+        }
+        for (int i = 0; i < 4; i++) {
+            int nextR = r + dr[i];
+            int nextC = c + dc[i];
+            if (nextR < 0 || nextR >= N || nextC < 0 || nextC >= M) {
+                continue;
+            }
+            if(!visited[nextR][nextC]){
+                visited[nextR][nextC] = true;
+                dfs(nextR, nextC, sum + num, depth + 1);
+                visited[nextR][nextC] = false;
+            }
+        }
 
-	static int go(int r, int c, int shape, int dir, int flip) {
-		int[] arr_r = new int[4];
-		int[] arr_c = new int[4];
-		if (shape == 1) {
-			int total = 0;
-			for (int i = 0; i < arr_r.length; i++) {
-				arr_r[i] = r + i * dr[dir];
-				arr_c[i] = c + i * dc[dir];
-			}
-			// 4가지 모든곳 다 정해졌다.
-			// 이제 이 모든 곳이 다 유효한 범위인지 확인해 줘야 한다.
-			for (int i = 0; i < arr_r.length; i++) {
-				if (arr_r[i] < 0 || arr_r[i] >= N || arr_c[i] < 0 || arr_c[i] >= M) {
-					return -1;
-				}
-			}
-			for (int i = 0; i < arr_r.length; i++) {
-				total += map[arr_r[i]][arr_c[i]];
-			}
-			return total;
-		} else if (shape == 2) {
-			int total = 0;
-			arr_r[0] = r;
-			arr_c[0] = c;
-			arr_r[1] = arr_r[0] + dr[dir];
-			arr_c[1] = arr_c[0] + dc[dir];
-			arr_r[2] = arr_r[1] + dr[(dir + 1) % 4];
-			arr_c[2] = arr_c[1] + dc[(dir + 1) % 4];
-			arr_r[3] = arr_r[2] + dr[(dir + 2) % 4];
-			arr_c[3] = arr_c[2] + dc[(dir + 2) % 4];
-			// 4가지 모든곳 다 정해졌다.
-			// 이제 이 모든 곳이 다 유효한 범위인지 확인해 줘야 한다.
-			for (int i = 0; i < arr_r.length; i++) {
-				if (arr_r[i] < 0 || arr_r[i] >= N || arr_c[i] < 0 || arr_c[i] >= M) {
-					return -1;
-				}
-			}
-			for (int i = 0; i < arr_r.length; i++) {
-				total += map[arr_r[i]][arr_c[i]];
-			}
-			return total;
-		} else if (shape == 3) {
-			int total = 0;
-			if (flip == 1) {
-				arr_r[0] = r;
-				arr_c[0] = c;
-				arr_r[1] = arr_r[0] + dr[dir];
-				arr_c[1] = arr_c[0] + dc[dir];
-				arr_r[2] = arr_r[1] + dr[dir];
-				arr_c[2] = arr_c[1] + dc[dir];
-				arr_r[3] = arr_r[2] + dr[(dir + 3) % 4];
-				arr_c[3] = arr_c[2] + dc[(dir + 3) % 4];
-			} else {
-				arr_r[0] = r;
-				arr_c[0] = c;
-				arr_r[1] = arr_r[0] + dr[dir];
-				arr_c[1] = arr_c[0] + dc[dir];
-				arr_r[2] = arr_r[1] + dr[dir];
-				arr_c[2] = arr_c[1] + dc[dir];
-				arr_r[3] = arr_r[2] + dr[(dir + 1) % 4];
-				arr_c[3] = arr_c[2] + dc[(dir + 1) % 4];
-			}
-			// 4가지 모든곳 다 정해졌다.
-			// 이제 이 모든 곳이 다 유효한 범위인지 확인해 줘야 한다.
-			for (int i = 0; i < arr_r.length; i++) {
-				if (arr_r[i] < 0 || arr_r[i] >= N || arr_c[i] < 0 || arr_c[i] >= M) {
-					return -1;
-				}
-			}
-			for (int i = 0; i < arr_r.length; i++) {
-				total += map[arr_r[i]][arr_c[i]];
-			}
-			return total;
-		} else if (shape == 4) {
-			int total = 0;
-			if (flip == 1) {
-				arr_r[0] = r;
-				arr_c[0] = c;
-				arr_r[1] = arr_r[0] + dr[dir];
-				arr_c[1] = arr_c[0] + dc[dir];
-				arr_r[2] = arr_r[1] + dr[(dir + 3) % 4];
-				arr_c[2] = arr_c[1] + dc[(dir + 3) % 4];
-				arr_r[3] = arr_r[2] + dr[dir];
-				arr_c[3] = arr_c[2] + dc[dir];
-			} else {
-				arr_r[0] = r;
-				arr_c[0] = c;
-				arr_r[1] = arr_r[0] + dr[dir];
-				arr_c[1] = arr_c[0] + dc[dir];
-				arr_r[2] = arr_r[1] + dr[(dir + 1) % 4];
-				arr_c[2] = arr_c[1] + dc[(dir + 1) % 4];
-				arr_r[3] = arr_r[2] + dr[dir];
-				arr_c[3] = arr_c[2] + dc[dir];
-			}
-			// 4가지 모든곳 다 정해졌다.
-			// 이제 이 모든 곳이 다 유효한 범위인지 확인해 줘야 한다.
-			for (int i = 0; i < arr_r.length; i++) {
-				if (arr_r[i] < 0 || arr_r[i] >= N || arr_c[i] < 0 || arr_c[i] >= M) {
-					return -1;
-				}
-			}
-			for (int i = 0; i < arr_r.length; i++) {
-				total += map[arr_r[i]][arr_c[i]];
-			}
-			return total;
-		} else {
-			int total = 0;
-			arr_r[0] = r;
-			arr_c[0] = c;
-			arr_r[1] = arr_r[0] + dr[dir];
-			arr_c[1] = arr_c[0] + dc[dir];
-			arr_r[2] = arr_r[1] + dr[(dir + 1) % 4];
-			arr_c[2] = arr_c[1] + dc[(dir + 1) % 4];
-			arr_r[3] = arr_r[1] + dr[dir];
-			arr_c[3] = arr_c[1] + dc[dir];
-			// 4가지 모든곳 다 정해졌다.
-			// 이제 이 모든 곳이 다 유효한 범위인지 확인해 줘야 한다.
-			for (int i = 0; i < arr_r.length; i++) {
-				if (arr_r[i] < 0 || arr_r[i] >= N || arr_c[i] < 0 || arr_c[i] >= M) {
-					return -1;
-				}
-			}
-			for (int i = 0; i < arr_r.length; i++) {
-				total += map[arr_r[i]][arr_c[i]];
-			}
-			return total;
-		}
-	}
+    }
+    public static void checkF(int r, int c){
+        for (int i = 0; i < 4; i++) {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		map = new int[N][M];
-		int max = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				map[i][j] = sc.nextInt();
-			}
-		}
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				// 모양
-				for (int j2 = 1; j2 <= 5; j2++) {
-					// 방향
-					for (int k = 0; k < 4; k++) {
-						// 플립
-						int tmp1 = go(i, j, j2, k, 1);
-						int tmp2 = go(i, j, j2, k, -1);
-						if (tmp1 != -1) {
-							max = Math.max(max, tmp1);
-						}
-						if (tmp2 != -1) {
-							max = Math.max(max, tmp2);
-						}
-					}
-				}
-			}
-		}
-		System.out.println(max);
-	}
+            int nextTwoR = r + dr[i] * 2;
+            int nextTwoC = c + dc[i] * 2;
+            if (nextTwoR < 0 || nextTwoR >= N || nextTwoC < 0 || nextTwoC >= M) {
+                continue;
+            }
+            int sideR = r + dr[i] + dr[(i + 1) % 4];
+            int sideC = c + dc[i] + dc[(i + 1) % 4];
+            int sum = 0;
+            if (!(sideR < 0 || sideR >= N || sideC < 0 || sideC >= M)) {
+                sum += map[r][c];
+                sum += map[r + dr[i]][c + dc[i]];
+                sum += map[nextTwoR][nextTwoC];
+                sum += map[sideR][sideC];
+            }
+            max = Math.max(sum, max);
+            sum = 0;
+            sideR = r + dr[i] + dr[(i + 3) % 4];
+            sideC = c + dc[i] + dc[(i + 3) % 4];
+            if (!(sideR < 0 || sideR >= N || sideC < 0 || sideC >= M)) {
+                sum += map[r][c];
+                sum += map[r + dr[i]][c + dc[i]];
+                sum += map[nextTwoR][nextTwoC];
+                sum += map[sideR][sideC];
+            }
+            max = Math.max(sum, max);
+        }
+
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+         N = Integer.parseInt(st.nextToken());
+         M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                visited[i][j] = true;
+                dfs(i, j, 0, 0);
+                visited[i][j] = false;
+                checkF(i, j);
+            }
+        }
+        System.out.println(max);
+    }
 }
