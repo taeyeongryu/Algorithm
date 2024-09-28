802446434c48ebf30cd6630a4d16c49192e95d40
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-09-28 13:49:12 +0900

    [Silver II] Title: 정수 삼각형, Time: 200 ms, Memory: 24472 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,48 +1,57 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+    static int[][] map;
+    static int[][] dp;
+    static int[] dr = {-1, -1};
+    static int[] dc = {-1, 0};
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		int[] pre = new int[1];
-		pre[0] = Integer.parseInt(br.readLine());
-		int[] cur=null;
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        map = new int[N+1][N+1];
+        dp = new int[N+1][N+1];
 
-		if (N==1) {
-			System.out.println(pre[0]);
-		} else {
-			for (int i = 2; i <= N; i++) {
-				cur = new int[i];
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < cur.length; j++) {
-					cur[j] = Integer.parseInt(st.nextToken());
-				}
-				for (int j = 0; j < i; j++) {
-					if (j == 0) {
-						cur[j] = pre[j] + cur[j];
-					} else if (j == i - 1) {
-						cur[j] = cur[j] + pre[j - 1];
-					} else {
-						cur[j] = Math.max(pre[j-1], pre[j]) + cur[j];
-					}
-				}
-				pre = cur;
-			}
-			int max = 0;
-			for (int i = 0; i < cur.length; i++) {
-				max = Math.max(max, cur[i]);
-			}
-			System.out.println(max);
-
-		}
-	}
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= i; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i = 1; i<=N; i++){
+            for (int j = 1; j <= i; j++) {
+                for (int k = 0; k < 2; k++) {
+                    int beforeR = i + dr[k];
+                    int beforeC = j + dc[k];
+                    dp[i][j] = Math.max(dp[beforeR][beforeC] + map[i][j], dp[i][j]);
+                }
+            }
+        }
+//        for (int i = 1; i <= N; i++) {
+//            for (int j = 1; j <= N; j++) {
+//                bw.append(map[i][j] + " ");
+//            }
+//            bw.append("\n");
+//        }
+//        for (int i = 1; i <= N; i++) {
+//            for (int j = 1; j <= N; j++) {
+//                bw.append(dp[i][j] + " ");
+//            }
+//            bw.append("\n");
+//        }
+        int result = 0;
+        for (int i = 1; i <=N ; i++) {
+            result = Math.max(result, dp[N][i]);
+        }
+        bw.append(result + "");
+        bw.close();
+        br.close();
+    }
 }
