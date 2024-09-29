0fbc269a0e0a98283a0a99f8c9f0d3a7c7edea41
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-09-29 13:00:51 +0900

    [Silver I] Title: 스티커, Time: 776 ms, Memory: 124288 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,41 +1,38 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
-		for (int t = 0; t < T; t++) {
-			int N = Integer.parseInt(br.readLine());
-			int[][] dp = new int[2][N + 1];
-			int[][] arr = new int[2][N + 1];
-			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
-				arr[0][j] = Integer.parseInt(st.nextToken());
-			}
-			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
-				arr[1][j] = Integer.parseInt(st.nextToken());
-			}
-			dp[0][1] = arr[0][1];
-			dp[1][1] = arr[1][1];
-			for (int i = 2; i <= N; i++) {
-				dp[0][i] = Math.max(dp[0][i - 2], dp[1][i - 2]) + arr[0][i];
-				dp[1][i] = Math.max(dp[0][i - 2], dp[1][i - 2]) + arr[1][i];
-				dp[0][i] = Math.max(dp[0][i], dp[1][i - 1] + arr[0][i]);
-				dp[1][i] = Math.max(dp[1][i], dp[0][i - 1] + arr[1][i]);
-			}
-			bw.write("" + Math.max(dp[0][N], dp[1][N]) + "\n");
-		}
-		bw.close();
-		br.close();
-	}
-
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            int[][] arr = new int[2][N + 1];
+            int[][] dp = new int[2][N + 1];
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < N + 1; j++) {
+                arr[0][j] = Integer.parseInt(st.nextToken());
+            }
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < N + 1; j++) {
+                arr[1][j] = Integer.parseInt(st.nextToken());
+            }
+            dp[0][1] = arr[0][1];
+            dp[1][1] = arr[1][1];
+            if(N!=1){
+                for (int j = 2; j < N + 1; j++) {
+                    dp[0][j] = Math.max(Math.max(dp[1][j - 1], dp[0][j - 2]), dp[1][j - 2]) + arr[0][j];
+                    dp[1][j] = Math.max(Math.max(dp[0][j - 1], dp[0][j - 2]), dp[1][j - 2]) + arr[1][j];
+                }
+            }
+            sb.append(Math.max(dp[0][N], dp[1][N]) + "\n");
+        }
+        System.out.println(sb.toString());
+        br.close();
+        
+    }
 }
