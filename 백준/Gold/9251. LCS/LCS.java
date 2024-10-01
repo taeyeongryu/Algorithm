f99aabfdc3f41f967c47a60585bc6eef51f54e27
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-10-01 18:32:04 +0900

    [Gold V] Title: LCS, Time: 112 ms, Memory: 16220 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,26 +1,35 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str1 = sc.next();
-		String str2 = sc.next();
-		int len1 = str1.length();
-		int len2 = str2.length();
-		int[][] DP = new int[len1 + 1][len2 + 1];
-		for (int i = 1; i <= len1; i++) {
-			for (int j = 1; j <= len2; j++) {
-				if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-					DP[i][j] = DP[i - 1][j - 1] + 1;
-				} else {
-					DP[i][j] = Math.max(DP[i - 1][j], DP[i][j - 1]);
-				}
-			}
-		}
-//		System.out.println(Arrays.deepToString(DP));
-		System.out.println(DP[len1][len2]);
-	}
+        String inputA = br.readLine();
+        String inputB = br.readLine();
 
+        int[][] dp = new int[inputA.length() + 1][inputB.length() + 1];
+        for (int i = 1; i <= inputA.length(); i++) {
+            for (int j = 1; j <= inputB.length(); j++) {
+                if(inputA.charAt(i-1)==inputB.charAt(j-1)){
+                    dp[i][j] = Math.max(Math.max(dp[i - 1][j - 1] + 1, dp[i][j - 1]), dp[i - 1][j]);
+                }else{
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j ]);
+                }
+            }
+        }
+//        for (int i = 0; i <dp.length ; i++) {
+//            for (int j = 0; j < dp[0].length; j++) {
+//                bw.append(dp[i][j] + " ");
+//            }
+//            bw.append("\n");
+//        }
+        bw.append(dp[inputA.length()][inputB.length()]+"");
+        bw.close();
+        br.close();
+
+    }
 }
