5126eafcca30f6659c47e20ce2d811ce52547e13
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-10-09 14:27:40 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 448 ms, Memory: 236188 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -1,62 +1,49 @@
 import java.util.Scanner;
 
 public class Main {
-    //기본이 3이 들어가야 한다.
-//    static void printThree(int space){
-//        StringBuilder sb = new StringBuilder();
-//
-//        for (int i = 0; i <space; i++) {
-//            sb.append(" ");
-//        }
-//        sb.append("*\n");
-//        for (int i = 0; i <space-1; i++) {
-//            sb.append(" ");
-//        }
-//        sb.append("* *\n");
-//        for (int i = 0; i <space-2; i++) {
-//            sb.append(" ");
-//        }
-//        sb.append("*****");
-//        System.out.print(sb.toString());
-//    }
+
     static char[][] starMap;
     //꼭짓점 별의 위치가 r,c에 들어간다.
-    static void print(int r, int c, int N){
-        if(N==3){
-            starMap[r][c] = '*';
+
+
+    static int N;
+    static int K;
+    static void appendStar(int r, int c, int height, int wide){
+        if(height==3){
+            starMap[r][c]='*';
             starMap[r + 1][c - 1] = '*';
             starMap[r + 1][c + 1] = '*';
-            starMap[r + 2][c - 1] = '*';
-            starMap[r + 2][c + 1] = '*';
             starMap[r + 2][c] = '*';
-            starMap[r + 2][c - 2] = '*';
+            starMap[r + 2][c + 1] = '*';
             starMap[r + 2][c + 2] = '*';
+            starMap[r + 2][c - 1] = '*';
+            starMap[r + 2][c - 2] = '*';
             return;
         }
-        else{
-            print(r,c,N/2);
-            print(r+N/2,c-(N/2),N/2);
-            print(r+N/2,c+(N/2),N/2);
-        }
-
+        appendStar(r , c, height / 2, wide / 2);
+        appendStar(r + height / 2, c - (wide / 4 + 1), height / 2, wide / 2);
+        appendStar(r + height / 2, c + (wide / 4 + 1), height / 2, wide / 2);
     }
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        starMap = new char[N][2*N - 1];
-        for (int i = 0; i <starMap.length ; i++) {
-            for (int j = 0; j < starMap[0].length; j++) {
-                starMap[i][j] = ' ';
+        N = sc.nextInt();
+        for (int i = 0; i < 10; i++) {
+            if(Math.pow(2,i)*3==N){
+                K = i;
+                break;
             }
         }
-        print(0,N-1,N);
+        starMap = new char[N][2*N-1];
+        appendStar(0, (2 * N - 1) / 2, N, 2 * N - 1);
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i <starMap.length ; i++) {
             for (int j = 0; j < starMap[0].length; j++) {
-                sb.append(starMap[i][j]);
+                sb.append(starMap[i][j] == '*' ? starMap[i][j] : ' ');
             }
             sb.append("\n");
         }
         System.out.println(sb.toString());
     }
+
 }
