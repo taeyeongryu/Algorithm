c464c0d9a324531223bd5a022731db09c4ecca44
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-10-09 10:20:58 +0900

    [Gold IV] Title: 최단경로, Time: 612 ms, Memory: 116820 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,60 +1,54 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static class Node implements Comparable<Node> {
+
+
         int index;
         int weight;
 
-        public Node(int index, int weight) {
+        Node(int index, int weight) {
             this.index = index;
             this.weight = weight;
         }
 
-        @Override
-        public int compareTo(Node o) {
-            return Integer.compare(weight, o.weight);
+        public int compareTo(Node n1){
+            return Integer.compare(this.weight, n1.weight);
         }
 
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "index=" + index +
-                    ", weight=" + weight +
-                    '}';
-        }
     }
-    static void dji(int start){
+    static int V, E;
+    static int[] dist;
+    static List<Node>[] adjList;
+    static void dij(int start){
+        dist[start] = 0;
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        dist[start]=0;
-        pq.add(new Node(start, 0));
+        pq.offer(new Node(start, 0));
 
         while (!pq.isEmpty()) {
-//            System.out.println(pq);
             Node cur = pq.poll();
+            int curIndex = cur.index;
+            int curWeight = cur.weight;
 
-            if(dist[cur.index]<cur.weight){
+            if(curWeight>dist[curIndex]){
                 continue;
             }
 
-            for (int i = 0; i <adjList[cur.index].size(); i++) {
-                Node next = adjList[cur.index].get(i);
-                if(dist[cur.index]+next.weight<dist[next.index]){
-                    dist[next.index] = dist[cur.index] + next.weight;
-                    pq.add(new Node(next.index, dist[next.index]));
+            for (int i = 0; i < adjList[curIndex].size(); i++) {
+                Node next = adjList[curIndex].get(i);
+                int nextIndex = next.index;
+                int nextWeight = next.weight;
+
+                if(dist[nextIndex]>curWeight+nextWeight){
+                    dist[nextIndex] = curWeight + nextWeight;
+                    pq.offer(new Node(nextIndex, curWeight + nextWeight));
                 }
             }
         }
     }
-    static int V, E;
-    static final int MAX_VALUE = Integer.MAX_VALUE;
-    static int[] dist;
-    static ArrayList<Node>[] adjList;
-
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -63,29 +57,32 @@ public class Main {
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
 
-        dist = new int[V + 1];
-
-        Arrays.fill(dist,MAX_VALUE);
+        int start = Integer.parseInt(br.readLine());
 
         adjList = new ArrayList[V + 1];
+        dist = new int[V + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
 
         for (int i = 0; i < adjList.length; i++) {
             adjList[i] = new ArrayList<>();
         }
 
-        int start = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            adjList[a].add(new Node(b, w));
+            adjList[s].add(new Node(e, w));
         }
-        dji(start);
-        for (int i = 1; i < V+1; i++) {
-            bw.write(dist[i] == MAX_VALUE ? "INF\n" : "" + dist[i] + "\n");
+        dij(start);
+        for (int i = 1; i < dist.length; i++) {
+            if(dist[i]==Integer.MAX_VALUE){
+                bw.append("INF\n");
+            }else{
+                bw.append(dist[i] + "\n");
+            }
         }
+
         bw.close();
         br.close();
     }
