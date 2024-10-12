3a43504488786ade106401ebb0575043f8a59ac8
TAE YEONG RYU <122500557+taeyeongryu@users.noreply.github.com>
2024-10-12 10:44:35 +0900

    [Gold III] Title: 음악프로그램, Time: 84 ms, Memory: 12548 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.java
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.java
@@ -1,67 +1,81 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.OutputStreamWriter;
+import java.util.*;
 
 public class Main {
 
-	static int N;
-	static int PD;
-	static int[] in;
-	static Queue<Integer> queue = new LinkedList<>();
-	static ArrayList<Integer>[] adjlist;
-	static ArrayList<Integer> resultlist = new ArrayList<>();
+    static int N, M;
+    static int[] inputCheck;
+    static List<Integer>[] adjList;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		PD = Integer.parseInt(st.nextToken());
-		in = new int[N + 1];
-		adjlist = new ArrayList[N + 1];
-		for (int i = 0; i <= N; i++) {
-			adjlist[i] = new ArrayList<>();
-		}
-		for (int i = 0; i < PD; i++) {
-			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int[] tmp = new int[n];
-			for (int j = 0; j < tmp.length; j++) {
-				tmp[j] = Integer.parseInt(st.nextToken());
-			}
-			for (int j = 0; j < tmp.length - 1; j++) {
-				adjlist[tmp[j]].add(tmp[j + 1]);
-				in[tmp[j + 1]]++;
-			}
-		}
-		for (int i = 1; i <= N; i++) {
-			if (in[i] == 0) {
-				queue.offer(i);
-			}
-		}
-		while (!queue.isEmpty()) {
-			int tmp = queue.poll();
-			resultlist.add(tmp);
-			for (int i = 0; i < adjlist[tmp].size(); i++) {
-				in[adjlist[tmp].get(i)]--;
-				if (in[adjlist[tmp].get(i)] == 0) {
-					queue.offer(adjlist[tmp].get(i));
-				}
-			}
-		}
-		if (resultlist.size()==N) {
-			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < resultlist.size(); i++) {
-				sb.append(resultlist.get(i)+"\n");
-			}
-			System.out.println(sb.toString());
-		}else {
-			System.out.println(0);
-		}
-	}
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
+        inputCheck = new int[N + 1];
+        adjList = new ArrayList[N + 1];
+
+        for (int i = 0; i < adjList.length; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        //pd숫자
+        for (int i = 0; i < M; i++) {
+            int before = -1;
+            st = new StringTokenizer(br.readLine());
+            int singerNum = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < singerNum; j++) {
+                int now = Integer.parseInt(st.nextToken());
+                if(before==-1){
+                    before = now;
+                    continue;
+                }
+                adjList[before].add(now);
+                inputCheck[now]++;
+                before = now;
+            }
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i < inputCheck.length; i++) {
+            if(inputCheck[i]==0){
+                queue.offer(i);
+            }
+        }
+        List<Integer> resultList = new ArrayList<>();
+        while(!queue.isEmpty()){
+            int cur = queue.poll();
+            resultList.add(cur);
+            for (int i = 0; i < adjList[cur].size(); i++) {
+                int next = adjList[cur].get(i);
+                inputCheck[next]--;
+                if(inputCheck[next]==0){
+                    queue.offer(next);
+                }
+            }
+        }
+        boolean flag = true;
+
+        for (int i = 0; i <inputCheck.length ; i++) {
+            if(inputCheck[i]!=0){
+                flag = false;
+                break;
+            }
+        }
+
+        if(flag){
+            for (int i = 0; i < resultList.size(); i++) {
+                bw.append(resultList.get(i) + "\n");
+            }
+        } else{
+            bw.append("0");
+        }
+        bw.close();
+        br.close();
+    }
 }
